1271498a8aa8809b1069a7a4db9f2608d5e3f0ee
erugis0345 <tl091264@gmail.com>
2019-01-17 00:24:33 +0900

    modify models

--- app/Team.php
+++ app/Team.php
@@ -6,8 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
+    protected $fillable = [
+            'name', 'avatar', 'slack_team_id',
+    ];
+
     public function teamUsers ()
     {
         return $this->hasMany('App\TeamUser', 'slack_team_id', 'slack_team_id');
+    }
+
+    public static function createWithTeamUser ($params)
+    {
+        $team = \App\Team::create($params);
+        \App\TeamUser::create(['oauth_id' => \Auth::user()->oauth_id, 'slack_team_id' => $team->slack_team_id, 'team_id' => $team->id]);
     }
 }
--- app/TeamUser.php
+++ app/TeamUser.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class TeamUser extends Model
 {
+    protected $fillable = [
+            'oauth_id', 'slack_team_id', 'team_id',
+    ];
+
     public function user ()
     {
         return $this->belongsTo('App\User', 'oauth_id', 'oauth_id');
@@ -16,15 +20,15 @@ class TeamUser extends Model
         return $this->belongsTo('App\Team', 'team_id', 'id');
     }
 
-    public function scopeSlackTeamId ($query)
+    public function scopeOauthId ($query, $oauth_id = null)
     {
-        dd($this->slack_team_id);
-//        return $query->where('slack_team_id', $slack_team_id);
+        $id = $oauth_id ?: \Auth::user()->oauth_id;
+        return $query->where('oauth_id', $id);
     }
 
-    public function scopeJoinedTeams ($query)
+    public function scopeSlackTeamId ($query, $slack_team_id = null)
     {
-        $id = \App\Slack::usersInfo()->team_id;
+        $id = $slack_team_id ?: \App\Slack::usersInfo()->team_id;
         return $query->where('slack_team_id', $id);
     }
 }
