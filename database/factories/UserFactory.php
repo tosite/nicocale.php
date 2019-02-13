89098f47423a1079842df9b5724f865232f50b29
erugis0345 <tl091264@gmail.com>
2019-02-13 22:00:07 +0900

    seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +16,18 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'name'           => $faker->name,
+        'slack_token'    => Str::Uuid(),
+        'slack_user_id'  => str_random(9),
+        'avatar'         => 'https://bsblog.casareal.co.jp/img/recruit.png',
+        'sns'            => 'slack',
         'remember_token' => str_random(10),
     ];
+    //    return [
+    //        'name' => $faker->name,
+    //        'email' => $faker->unique()->safeEmail,
+    //        'email_verified_at' => now(),
+    //        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+    //        'remember_token' => str_random(10),
+    //    ];
 });
