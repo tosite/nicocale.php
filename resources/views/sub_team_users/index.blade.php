1b22cfef8448dcf2437191352d35a9887ec8d0f0
erugis0345 <tl091264@gmail.com>
2019-03-23 01:28:52 +0900

    ビューファイルに情報を追加しました

--- resources/views/sub_team_users/index.blade.php
+++ resources/views/sub_team_users/index.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Sub team User index</h1>
-    <?php $d = new \DateTime(); ?>
-    @foreach($subTeamUsers as $stu)
-        <p>
-            <a href="{{ route('team_users.calendar', ['teamUserId' => $stu->team_user_id, 'year'=> $d->format('Y'), 'month'=> $d->format('n')]) }}">{{ $stu->user->name }}</a>
-        </p>
-    @endforeach
+  <h1>Sub team User index</h1>
+  <sub-team-user-list
+    :users="{{ json_encode($subTeamUsers) }}"
+    :me="{{ json_encode($me) }}"
+  ></sub-team-user-list>
 @endsection
