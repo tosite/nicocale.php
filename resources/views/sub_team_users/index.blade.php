56b1506878667111804e3321939ba9f8aca8dfb8
erugis0345 <tl091264@gmail.com>
2019-03-22 22:43:38 +0900

    ルーティングを追加しました

--- resources/views/sub_team_users/index.blade.php
+++ resources/views/sub_team_users/index.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
     <h1>Sub team User index</h1>
+    <?php $d = new \DateTime(); ?>
     @foreach($subTeamUsers as $stu)
-        <p>{{ $stu->teamUser->user->name }}</p>
+        <p>
+            <a href="{{ route('team_users.calendar', ['teamUserId' => $stu->team_user_id, 'year'=> $d->format('Y'), 'month'=> $d->format('n')]) }}">{{ $stu->user->name }}</a>
+        </p>
     @endforeach
 @endsection
