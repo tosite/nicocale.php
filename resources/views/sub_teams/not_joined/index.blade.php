d8c660d0d4e86c9c703e6be3c1a11b3778f13535
erugis0345 <tl091264@gmail.com>
2019-03-23 15:55:40 +0900

    不要なコメントを除去しました

--- resources/views/sub_teams/not_joined/index.blade.php
+++ resources/views/sub_teams/not_joined/index.blade.php
@@ -3,7 +3,4 @@
 @section('content')
     <h1>Not joined sub teams</h1>
     <not-joined-sub-team-list :not-joined-sub-teams="{{ json_encode($notJoinedSubTeams) }}"></not-joined-sub-team-list>
-    {{--@foreach($notJoinedSubTeams as $row)--}}
-        {{--<p>{{ $row->name }}</p>--}}
-    {{--@endforeach--}}
 @endsection
