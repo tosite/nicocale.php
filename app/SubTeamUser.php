012e6e21f9187d85ecc518620915bc7aa0129d67
erugis0345 <tl091264@gmail.com>
2019-03-24 09:04:08 +0900

    Modelを修正しました

--- app/SubTeamUser.php
+++ app/SubTeamUser.php
@@ -27,10 +27,11 @@ class SubTeamUser extends Model
     public function team     () { return $this->belongsTo('App\Team', 'team_id', 'id'); }
     public function user     () { return $this->belongsTo('App\User', 'user_id', 'id'); }
 
-    public function scopeUserId     ($query, $userId = null) { return $query->where(['user_id' => $userId ?: \Auth::user()->id]); }
-    public function scopeTeamId     ($query, $teamId)        { return $query->where(['team_id' => $teamId]); }
-    public function scopeSubTeamId  ($query, $subTeamId)     { return $query->where(['sub_team_id' => $subTeamId]); }
-    public function scopeTeamUserId ($query, $id = null)     { return $query->where(['team_user_id' => $id ?: \Auth::user()->id]); }
+    public function scopeMe  ($query)                     { return $query->userId(\Auth::user()->id); }
+    public function scopeUserId     ($query, $userId)     { return $query->where(['user_id' => $userId]); }
+    public function scopeTeamId     ($query, $teamId)     { return $query->where(['team_id' => $teamId]); }
+    public function scopeSubTeamId  ($query, $subTeamId)  { return $query->where(['sub_team_id' => $subTeamId]); }
+    public function scopeTeamUserId ($query, $teamUserId) { return $query->where(['team_user_id' => $teamUserId]); }
 
     public static function findOrCreate($sub_team_id) {
         $subTeam  = \App\SubTeam::find($sub_team_id);
