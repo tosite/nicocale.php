1aa9ea0e329dc0fd82ac7c225468e777dc8406a1
erugis0345 <tl091264@gmail.com>
2019-04-29 20:31:46 +0900

    not joined sub teams

--- resources/js/components/NotJoinedSubTeamList.vue
+++ resources/js/components/NotJoinedSubTeamList.vue
@@ -1,28 +1,46 @@
 <template>
-  <v-card>
-    <v-list two-line subheader class="pb-0">
+  <v-layout row>
+    <v-flex xs12 sm8 offset-sm2>
+      <v-card>
 
-      <div v-for="subTeam in notJoinedSubTeams">
-        <v-list-tile :key="subTeam.id" avatar>
-          <v-list-tile-avatar>
-            <img :src="subTeam.avatar">
-          </v-list-tile-avatar>
+        <v-toolbar color="primary" dark>
+          <v-toolbar-title>未参加のサブチーム</v-toolbar-title>
+        </v-toolbar>
+        <v-alert
+          :value="true"
+          color="warning"
+          icon="priority_high"
+          class="mt-0"
+          outline
+          v-if="notJoinedSubTeams == false"
+        >
+          未参加のサブチームはありません。
+        </v-alert>
 
-          <v-list-tile-content>
-            <v-list-tile-title>{{ subTeam.name }}</v-list-tile-title>
-            <v-list-tile-sub-title>{{ subTeam.bio }}</v-list-tile-sub-title>
-          </v-list-tile-content>
+        <v-list two-line subheader class="pb-0" v-else>
+          <div v-for="subTeam in notJoinedSubTeams">
+            <v-list-tile :key="subTeam.id" avatar>
+              <v-list-tile-avatar>
+                <img :src="subTeam.avatar">
+              </v-list-tile-avatar>
 
-          <v-list-tile-action>
-            <v-btn icon ripple @click.stop="">
-              <v-icon color="grey lighten-1">add_circle_outline</v-icon>
-            </v-btn>
-          </v-list-tile-action>
-        </v-list-tile>
-        <v-divider class="ma-0"></v-divider>
-      </div>
-    </v-list>
-  </v-card>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ subTeam.name }}</v-list-tile-title>
+                <v-list-tile-sub-title>{{ subTeam.bio }}</v-list-tile-sub-title>
+              </v-list-tile-content>
+
+              <v-list-tile-action>
+                <v-btn icon ripple @click.stop="">
+                  <v-icon color="grey lighten-1">add_circle</v-icon>
+                </v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-divider class="ma-0"></v-divider>
+          </div>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
