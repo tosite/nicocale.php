28ba004bc5576d6dce4260635d0786dc1c6141f2
erugis0345 <tl091264@gmail.com>
2019-05-01 00:35:18 +0900

    update

--- app/Http/Controllers/EmotionController.php
+++ app/Http/Controllers/EmotionController.php
@@ -8,7 +8,13 @@ class EmotionController extends Controller
     public function store(\App\Http\Requests\Emotions\Post $request)
     {
         $user = \Auth::user();
-        $params = array_merge(['user_id' => $user->id], $request->input());
+        $input = $request->only(['team_id', 'team_user_id', 'emoji', 'status_text', 'memo', 'entered_on']);
+        $params = array_merge(['user_id' => $user->id], $input);
+
+        if (!$this->isTeamExist($params['team_id'], $user->id)) {
+            throw new \Exception('不正なアクセスです。');
+        }
+
         return response(\App\Emotion::create($params), 201);
     }
 
@@ -16,7 +22,17 @@ class EmotionController extends Controller
     {
         $emotion = \App\Emotion::find($emotionId);
         $params = $request->only(['emoji', 'status_text', 'memo']);
+
+        if (!$this->isTeamExist($emotion->team_id, $emotion->user_id)) {
+            throw new \Exception('不正なアクセスです。');
+        }
+
         $emotion->fill($params)->save();
         return response($emotion, 200);
+    }
+
+    private function isTeamExist($teamId, $userId) : bool
+    {
+        return \App\TeamUser::teamId($teamId)->userId($userId)->exists();
     }
 }
