4e48c20620a6fb17c93083375da88ea098d2895a
erugis0345 <tl091264@gmail.com>
2019-04-30 17:34:06 +0900

    locale

--- resources/js/components/TeamUsers/Calendar.vue
+++ resources/js/components/TeamUsers/Calendar.vue
@@ -1,8 +1,10 @@
 <template>
   <v-layout wrap>
     <v-flex xs12 v-if="today != null">
+      <h1>{{ today | month }}</h1>
+      <v-date-picker v-model="currentMonth" type="month" color="primary" locale="jp-ja"></v-date-picker>
       <v-sheet height="600">
-        <v-calendar :now="today" :value="today" color="primary">
+        <v-calendar :now="today" :value="today" color="primary" locale="jp-ja">
           <template v-slot:day="{ present, past, date }">
             <template v-if="emotions[date] != null && emotions[date].user != null">
               <p class="text-xs-center mb-0 mt-2">
@@ -22,9 +24,16 @@
     props: ['emotions'],
     data: () => ({
       today: null,
+      currentMonth: null,
     }),
     created() {
       this.today = dayjs().format('YYYY-MM-DD');
+      this.currentMonth = dayjs().format('YYYY-MM');
+    },
+    filters: {
+      month: function (date) {
+        return dayjs(date).format('YYYY年M月');
+      },
     },
   }
 </script>
--- resources/views/team_users/calendars/index.blade.php
+++ resources/views/team_users/calendars/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Team user emotion calendar</h1>
   <team-user-calendar :emotions="{{ json_encode($emotions) }}"></team-user-calendar>
 @endsection
