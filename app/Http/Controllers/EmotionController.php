57e1c9a52637446c654f8afd4ad2a47922a917b6
erugis0345 <tl091264@gmail.com>
2019-05-01 00:00:07 +0900

    store

--- app/Http/Controllers/EmotionController.php
+++ app/Http/Controllers/EmotionController.php
@@ -7,6 +7,9 @@ class EmotionController extends Controller
 
     public function store(\App\Http\Requests\Emotions\Post $request)
     {
+        $user = \AUth::user();
+        $params = array_merge(['user_id' => $user->id], $request->input());
+        return response(\App\Emotion::create($params), 201);
     }
 
     public function update(\App\Http\Requests\Emotions\Put $request, $emotionId)
---
+++ tests/Controller/EmotionControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class EmotionControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+    protected $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->user = \App\User::first();
+    }
+
+    public function test_store_データ不備()
+    {
+        $params = [
+            'team_id' => $this->user->id,
+        ];
+
+        $this->actingAs($this->user, 'api')
+            ->json('POST', '/emotions', $params)
+            ->assertStatus(422);
+    }
+
+    public function test_store_新規データ()
+    {
+        $emotion = \App\Emotion::userId($this->user->id)->first();
+        $params = [
+            'team_id' => $emotion->team_id,
+            'team_user_id' => $emotion->team_user_id,
+            'emoji' => ':smile:',
+            'status_text' => 'happy',
+            'memo' => $emotion->memo,
+            'entered_on' => '2000-01-01',
+        ];
+
+        $this->actingAs($this->user, 'api')
+            ->json('POST', '/emotions', $params)
+            ->assertStatus(201);
+    }
+
+    public function test_store_すでに存在しているデータ()
+    {
+        $emotion = \App\Emotion::userId($this->user->id)->first();
+        $params = [
+            'team_id' => $emotion->team_id,
+            'team_user_id' => $emotion->team_user_id,
+            'emoji' => $emotion->emoji,
+            'status_text' => $emotion->status_text,
+            'memo' => $emotion->memo,
+            'entered_on' => $emotion->entered_on,
+        ];
+        $this->actingAs($this->user, 'api')
+            ->json('POST', '/emotions', $params)
+            ->assertStatus(500);
+//        dd($res->baseResponse->getContent());
+    }
+}
