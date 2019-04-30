909220dea02f79a0e8982730efbbd2aef85015d8
erugis0345 <tl091264@gmail.com>
2019-04-30 17:22:31 +0900

    add calendar-component

---
+++ resources/js/components/TeamUsers/Calendar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout wrap>
+    <v-flex xs12 v-if="today != null">
+      <v-sheet height="600">
+        <v-calendar :now="today" :value="today" color="primary">
+          <template v-slot:day="{ present, past, date }">
+            <template v-if="emotions[date] != null && emotions[date].user != null">
+              <p class="text-xs-center mb-0 mt-2">
+                <emotion-popper :emotion="emotions[date].user" :size="48"></emotion-popper>
+              </p>
+            </template>
+          </template>
+        </v-calendar>
+      </v-sheet>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+  export default {
+    props: ['emotions'],
+    data: () => ({
+      today: null,
+    }),
+    created() {
+      this.today = dayjs().format('YYYY-MM-DD');
+    },
+  }
+</script>
+
+<style scoped>
+  .v-calendar { border: 1px solid #ddd !important; }
+</style>
--- resources/views/team_users/calendars/index.blade.php
+++ resources/views/team_users/calendars/index.blade.php
@@ -2,42 +2,5 @@
 
 @section('content')
   <h1>Team user emotion calendar</h1>
-  <table id="calendar">
-    <thead>
-    <tr>
-      @foreach (['日', '月', '火', '水', '木', '金', '土'] as $dayOfWeek)
-        <th>{{ $dayOfWeek }}</th>
-      @endforeach
-    </tr>
-    </thead>
-    <tbody>
-    @foreach ($emotions as $e)
-        <?php $date = $e['date']; ?>
-        @if ($date->dayOfWeek == 0)
-          <tr>
-            @endif
-            <td
-              @if ($date->month != $month->format('m'))
-              class="grey lighten-4"
-              @endif
-            >
-              {{ $date->day }}
-              @if($date->month != $month->format('m'))
-              @elseif($isMe)
-                <emotion-modal
-                  :date="'{{ $date->format('n月j日') }}'"
-                  :emotion="{{ json_encode($e['user']) }}"
-                ></emotion-modal>
-              @else
-                <emotion-popper
-                  :emotion="{{ json_encode($e['user']) }}"
-                ></emotion-popper>
-              @endif
-            </td>
-            @if ($date->dayOfWeek == 6)
-          </tr>
-        @endif
-    @endforeach
-    </tbody>
-  </table>
+  <team-user-calendar :emotions="{{ json_encode($emotions) }}"></team-user-calendar>
 @endsection
