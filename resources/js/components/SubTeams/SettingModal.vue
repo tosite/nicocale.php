9577d82f49a4ae1d923c5f4ed50caa3e249514e0
erugis0345 <tl091264@gmail.com>
2019-05-04 18:54:32 +0900

    add snackbar

--- resources/js/components/SubTeams/SettingModal.vue
+++ resources/js/components/SubTeams/SettingModal.vue
@@ -1,69 +1,72 @@
 <template>
-  <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-    <template v-slot:activator="{ on }">
-      <v-btn flat icon color="accent" v-on="on">
-        <v-icon>info</v-icon>
-      </v-btn>
-    </template>
+  <div>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn flat icon color="accent" v-on="on">
+          <v-icon>info</v-icon>
+        </v-btn>
+      </template>
 
-    <v-card>
-      <div>
-        <v-toolbar color="primary" dark tabs>
-          <v-btn icon dark @click="dialog = false">
-            <v-icon>close</v-icon>
-          </v-btn>
+      <v-card>
+        <div>
+          <v-toolbar color="primary" dark tabs>
+            <v-btn icon dark @click="dialog = false">
+              <v-icon>close</v-icon>
+            </v-btn>
 
-          <v-toolbar-title>Settings</v-toolbar-title>
+            <v-toolbar-title>Settings</v-toolbar-title>
 
-          <template v-slot:extension>
-            <v-tabs
-              v-model="tab"
-              color="primary"
-              slider-color="accent"
-            >
-              <v-tab key="0" href="#tab-0">メンバー</v-tab>
-              <v-tab key="1" href="#tab-1">参加していないメンバー</v-tab>
-              <v-tab key="2" href="#tab-2">設定</v-tab>
-            </v-tabs>
-          </template>
+            <template v-slot:extension>
+              <v-tabs
+                v-model="tab"
+                color="primary"
+                slider-color="accent"
+              >
+                <v-tab key="0" href="#tab-0">メンバー</v-tab>
+                <v-tab key="1" href="#tab-1">参加していないメンバー</v-tab>
+                <v-tab key="2" href="#tab-2">設定</v-tab>
+              </v-tabs>
+            </template>
 
-        </v-toolbar>
+          </v-toolbar>
 
-        <v-layout row wrap>
-          <v-flex xs12 sm8 offset-sm2 class="pt-2">
-            <v-tabs-items v-model="tab">
-              <v-tab-item key="0" value="tab-0">
-                <joined-user-tab
-                  :users="joinedUsers"
-                ></joined-user-tab>
-              </v-tab-item>
+          <v-layout row wrap>
+            <v-flex xs12 sm8 offset-sm2 class="pt-2">
+              <v-tabs-items v-model="tab">
+                <v-tab-item key="0" value="tab-0">
+                  <joined-user-tab
+                    :users="joinedUsers"
+                  ></joined-user-tab>
+                </v-tab-item>
 
-              <v-tab-item key="1" value="tab-1">
-                <v-card flat>
-                  <not-joined-user-tab
-                    :users="notJoinedUsers"
-                    :subTeam="subTeam"
-                  ></not-joined-user-tab>
-                </v-card>
-              </v-tab-item>
+                <v-tab-item key="1" value="tab-1">
+                  <v-card flat>
+                    <not-joined-user-tab
+                      :users="notJoinedUsers"
+                      :subTeam="subTeam"
+                    ></not-joined-user-tab>
+                  </v-card>
+                </v-tab-item>
 
-              <v-tab-item key="2" value="tab-2">
-                <v-card flat>
-                  <setting-tab
-                    :sub-team="subTeam"
-                    :subTeamUser="subTeamUser"
-                  ></setting-tab>
-                </v-card>
-              </v-tab-item>
+                <v-tab-item key="2" value="tab-2">
+                  <v-card flat>
+                    <setting-tab
+                      :sub-team="subTeam"
+                      :subTeamUser="subTeamUser"
+                    ></setting-tab>
+                  </v-card>
+                </v-tab-item>
 
-            </v-tabs-items>
-          </v-flex>
-        </v-layout>
-      </div>
+              </v-tabs-items>
+            </v-flex>
+          </v-layout>
+        </div>
 
-    </v-card>
+      </v-card>
 
-  </v-dialog>
+    </v-dialog>
+    <snackbar :snackbar="snackbar" @closeSnackbar="closeSnackbar"></snackbar>
+  </div>
 </template>
 
 <script>
@@ -87,9 +90,17 @@
         notJoinedUsers: null,
         subTeam: null,
         subTeamUser: null,
+        snackbar: {
+          open: false,
+          type: '',
+          text: '',
+        },
       }
     },
     methods: {
+      closeSnackbar: function () {
+        this.snackbar.open = false;
+      },
       fetchParams: function () {
         axios.get(`/api/v1/sub-teams/${this.subTeamId}/info-modals`).then(res => {
           this.joinedUsers = res.data.joinedUsers;
