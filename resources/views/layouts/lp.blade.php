c33b2c194ac3181a471e71f88da35b1df15c1980
erugis0345 <tl091264@gmail.com>
2019-05-21 00:58:05 +0900

    add button

--- resources/views/layouts/lp.blade.php
+++ resources/views/layouts/lp.blade.php
@@ -9,6 +9,16 @@
     <v-toolbar dark color="primary">
       <v-toolbar-title class="white--text">NicoCale</v-toolbar-title>
       <v-spacer></v-spacer>
+      <a
+        href="https://slack.com/oauth/authorize?client_id=306106578305.492917770354&scope=emoji:read,users.profile:read,users.profile:write"
+      >
+        <img alt="Add to Slack"
+             height="40"
+             width="139"
+             src="https://platform.slack-edge.com/img/add_to_slack.png"
+             srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+        >
+      </a>
     </v-toolbar>
 
     <v-content>
