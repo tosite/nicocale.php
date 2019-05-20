15e6f260f04d7a1198efbd805cc690ba9f7dc61c
erugis0345 <tl091264@gmail.com>
2019-05-21 00:50:31 +0900

    split header

--- resources/views/layouts/lp.blade.php
+++ resources/views/layouts/lp.blade.php
@@ -1,15 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-
-  <title>{{ config('app.name', 'Laravel') }}</title>
-  <script src="{{ secure_asset('js/app.js') }}" defer></script>
-  <script>window.Laravel = {csrfToken: "{{ csrf_token() }}"};</script>
-  <link rel="dns-prefetch" href="//fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
+  @include('layouts.header')
 </head>
 <body>
 <div id="app">
