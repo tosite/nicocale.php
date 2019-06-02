7e1c6a3db58df7a8df1caf50e256b389c688f1b4
erugis0345 <tl091264@gmail.com>
2019-06-02 14:19:01 +0900

    add request

--- app/Http/Requests/Emotions/Post.php
+++ app/Http/Requests/Emotions/Post.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Emotions;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class Post extends FormRequest
 {
@@ -13,10 +14,12 @@ class Post extends FormRequest
 
     public function rules()
     {
+        $score = collect(\App\Emotion::SCORE)->pluck('value')->toArray();
         return [
             'team_id' => 'required|exists:teams,id',
             'team_user_id' => 'required|exists:team_users,id',
             'emoji' => 'required|string',
+            'score' => ['required', Rule::in($score)],
             'status_text' => 'string|nullable|max:100',
             'memo' => 'string|nullable',
             'entered_on' => 'required|date',
@@ -29,6 +32,7 @@ class Post extends FormRequest
             'team_id' => 'チームID',
             'team_user_id' => 'チームユーザーID',
             'emoji' => 'Emoji',
+            'score' => '気分',
             'status_text' => 'ステータステキスト',
             'memo' => 'メモ',
             'entered_on' => '日付',
